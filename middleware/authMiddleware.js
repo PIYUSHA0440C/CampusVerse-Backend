98bf5317d164555a44675b90e7ef917c715cf0ae
PIYUSH SHRIVASTAV <piyushshrivastav106@gmail.com>
2025-07-23 19:02:48 +0530

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -3,13 +3,12 @@ const jwt = require('jsonwebtoken')
 
 exports.protect = (req, res, next) => {
   const token = req.cookies.token
-  if (!token) return res.status(401).json({ message: 'Unauthorized' })
-
+  if (!token) return res.status(401).json({ message:'Unauthorized' })
   try {
     const payload = jwt.verify(token, process.env.JWT_SECRET)
     req.userId = payload.id
     next()
   } catch {
-    res.status(401).json({ message: 'Invalid token' })
+    res.status(401).json({ message:'Invalid token' })
   }
 }
