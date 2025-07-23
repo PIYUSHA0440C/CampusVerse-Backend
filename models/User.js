8099f3405f620906aa64761d0b012d51ee54628e
PIYUSH SHRIVASTAV <piyushshrivastav106@gmail.com>
2025-07-23 20:07:20 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,12 +1,11 @@
 // models/User.js
-const mongoose = require('mongoose')
-const { Schema } = mongoose
+const mongoose = require('mongoose');
 
-const userSchema = new Schema({
-  username: { type:String, unique:true, required:true },
-  email:    { type:String, unique:true, required:true },
-  password: { type:String, required:true },
-  college:  { type:String, required:true }
-})
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true, trim: true },
+  email:    { type: String, required: true, unique: true, trim: true, lowercase: true },
+  college:  { type: String, required: true, trim: true },
+  password: { type: String, required: true }
+}, { timestamps: true });
 
-module.exports = mongoose.model('User', userSchema)
+module.exports = mongoose.model('User', userSchema);
