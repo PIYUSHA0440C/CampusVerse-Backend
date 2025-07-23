59c2105fcbd516fed549a3f0d589657e990c0590
PIYUSH SHRIVASTAV <piyushshrivastav106@gmail.com>
2025-07-23 18:45:00 +0530

    Update Chat.js

--- models/Chat.js
+++ models/Chat.js
@@ -3,33 +3,12 @@ const mongoose = require('mongoose')
 const { Schema } = mongoose
 
 const chatSchema = new Schema({
-  kind: {
-    type: String,
-    enum: ['global','group','one-to-one'],
-    required: true
-  },
-  sender: {
-    type: Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
-  },
-  // for group chat:
-  college: {
-    type: String
-  },
-  // for one-to-one:
-  receiver: {
-    type: Schema.Types.ObjectId,
-    ref: 'User'
-  },
-  text: {
-    type: String,
-    required: true
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
-  }
+  kind:     { type: String, enum: ['global','group','one-to-one'], required: true },
+  sender:   { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  college:  { type: String },   // for group chat
+  receiver: { type: Schema.Types.ObjectId, ref: 'User' }, // for one-to-one
+  text:     { type: String, required: true },
+  createdAt:{ type: Date, default: Date.now }
 })
 
 module.exports = mongoose.model('Chat', chatSchema)
